6a1f3d00719fe76f6147d9ab63b7b029843a385f
Sarah Morard <153022338+sarahmariamorard@users.noreply.github.com>
2024-01-04 19:33:20 +0100

    Update universal.js

--- static/js/universal.js
+++ static/js/universal.js
@@ -70,6 +70,7 @@ function onPointClick(e) {
 
 // Load itineraries asynchronously using Fetch API.
 var cedric_itineraries = "static/data/cedric_itineraries.geojson";
+var vtt_itineraries = "static/data/VTT_Cabane_CAS_propositions_SM.geojson";
 
 var a_itineraries = fetch(
   cedric_itineraries
