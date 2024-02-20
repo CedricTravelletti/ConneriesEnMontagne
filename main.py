40a1e705ab9c9231e86fd385062749b66209b4d6
CedricTravelletti <cedrictravelletti@gmail.com>
2024-02-20 20:47:07 +0100

    Added Binntal itinerary.

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ app=Flask(__name__)
 def root():
     return render_template('index.html')
 
+@app.route('/itineraire_binntal_2jours')
+def itineraire_binntal_2jours():
+    return render_template('binntal-ski-2-jours.html')
+
+
 if __name__ == '__main__':
     app.run(host="localhost", port=8080, debug=True)
